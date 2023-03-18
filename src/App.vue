47145d93e1a0f9132a40a71101926ac7323cf6d1
Maxime <Maxime.paget26@gmail.com>
2023-03-18 20:00:37 +0100

    ajout de la navbar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script>
     import CardList from './components/CardList.vue';
+    import Header from './components/Header.vue';
 
     export default {
         name: "App",
@@ -9,13 +10,16 @@
             }
         },
         components: {
-            CardList
+            CardList,
+            Header
         },
     }
 </script>
 
 <template>
-    <CardList :cards="[
+    <Header></Header>
+    <main class="container">
+        <CardList :title="'Mes playlist'" :cards="[
                 {
                     title: 'FEUR',
                     content: 'feur content'
@@ -33,4 +37,41 @@
                     content: 'feur content'
                 }
             ]"/>
+        <CardList :title="'Album en tendance'" :cards="[
+                {
+                    title: 'FEUR',
+                    content: 'feur content'
+                },
+                {
+                    title: 'FEUR',
+                    content: 'feur content'
+                },
+                {
+                    title: 'FEUR',
+                    content: 'feur content'
+                },
+                {
+                    title: 'FEUR',
+                    content: 'feur content'
+                }
+            ]"/>
+        <CardList :title="'Titres les plus écoutés'" :cards="[
+            {
+                title: 'FEUR',
+                content: 'feur content'
+            },
+            {
+                title: 'FEUR',
+                content: 'feur content'
+            },
+            {
+                title: 'FEUR',
+                content: 'feur content'
+            },
+            {
+                title: 'FEUR',
+                content: 'feur content'
+            }
+        ]"/>
+    </main>
 </template>
