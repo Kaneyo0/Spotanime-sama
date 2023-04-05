dfc58221289ce15de26739d033dde4e680657d90
Maxime <Maxime.paget26@gmail.com>
2023-04-05 20:53:47 +0200

    ajout du toggle

--- src/components/InlineCard.vue
+++ src/components/InlineCard.vue
@@ -10,10 +10,16 @@
             image: {
                 type: String,
                 default: "none" // default value to remove the automatic border which is add by chrome when the src attribute is empty
-            },
-            edited: {
-                type: Boolean,
-                default: false
+            }
+        },
+        data() {
+            return {
+                edited: true
+            }
+        },
+        methods: {
+            toggleEdit() {
+                this.edited = !this.edited
             }
         }
     }
@@ -22,12 +28,12 @@
     <div :data-id=id class="inline-card" v-if="!edited">
         <img :src="image" class="inline-card__img">
         <p class="inline-card__title"> {{ title }}</p>
-        <i class="contact__icon material-symbols-outlined">edit</i>
+        <i class="contact__icon material-symbols-outlined" @click="toggleEdit">edit</i>
         <i class="contact__icon material-symbols-outlined">delete</i>
     </div>
     <div :data-id=id class="inline-card" v-else>
         <img :src="image" class="inline-card__img">
         <input type="text" class="inline-card__title" :placeholder="title"> 
-        <i class="contact__icon material-symbols-outlined">done</i>
+        <i class="contact__icon material-symbols-outlined" @click="toggleEdit">done</i>
     </div>
 </template>
